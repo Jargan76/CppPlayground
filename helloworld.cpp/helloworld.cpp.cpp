2fb71c8e3659f88582cdd0415d3fa11f6f9b3d39
Eric Hebel <Jargan76Studio@gmail.com>
2017-01-21 10:39:36 -0500

    first c++ program

--- helloworld.cpp/helloworld.cpp.cpp
+++ helloworld.cpp/helloworld.cpp.cpp
@@ -2,10 +2,36 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+
 
 
 int main()
 {
+	// good old Hello World!!
+	std::cout << "Hello" << std::endl;
+	std::cout << "Eric!" << std::endl;
+
+	// little simple math
+	int x = 5;
+	x = x - 2;
+	std::cout << "x = " << x << std::endl; // answer is 3
+	int y = x; // y is now 3
+	std::cout <<"y = " << y << std::endl; // answer is 3
+	std::cout <<"x + y = "<< x + y << std::endl; // answer 6
+
+	// pick a number 
+	std::cout <<"Enter a number: "<< std::endl;
+	// seting variable
+	int i;
+	// read number from console and assigns to x
+	std::cin >> i;
+	// responce from input
+	std::cout <<"Ta Daa.. The number you picked was "<< i + i << std::endl;
+	std::cout << "Just kidding. The real number is " << i << std::endl;
     return 0;
+
+
 }
 
